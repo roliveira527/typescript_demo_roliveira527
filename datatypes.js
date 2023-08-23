0fe9946448064b4cc342780fa4f481b418fe0332
Ricardo Oliveira <r.deoliveira527@gmail.com>
2023-08-23 16:18:46 +0100

    fixed parseInt, added comments and added boolean

--- datatypes.js
+++ datatypes.js
@@ -1,6 +1,22 @@
 "use strict";
+// String
 let lname;
 lname = "Oliveira";
-// lname = 10;
+// lname = 10; Type 'number' not assignable to type 'string'
 let upCase = lname.toUpperCase();
-console.log(upCase);
+console.log(upCase); // Print: OLIVEIRA
+// Number
+let age;
+age = 25;
+age = 25.5; // Decimals are allowed
+// age = "25"; Type 'string' not assignable to type 'number'
+console.log(age); // Print: 25.5
+// Parse String -> Number
+let dob = "12121999";
+let result = parseInt(dob);
+console.log(result); // Print: 12121999
+// Boolean
+let isValid;
+// console.log(isValid); ERR: isValid is used before being assigned
+isValid = false;
+console.log(isValid);
--- datatypes.ts
+++ datatypes.ts
@@ -1,15 +1,24 @@
+// String
 let lname : string;
 lname = "Oliveira";
-// lname = 10;
+// lname = 10; Type 'number' not assignable to type 'string'
 let upCase = lname.toUpperCase();
-console.log(upCase);
+console.log(upCase); // Print: OLIVEIRA
 
+// Number
 let age : number;
 age = 25;
-age = 25.5;
-// age = "25";
+age = 25.5; // Decimals are allowed
+// age = "25"; Type 'string' not assignable to type 'number'
+console.log(age); // Print: 25.5
 
+// Parse String -> Number
 let dob = "12121999";
+let result = parseInt(dob);
+console.log(result); // Print: 12121999
 
-let result = parseInt("dob");
-console.log(result);
+// Boolean
+let isValid : boolean;
+// console.log(isValid); ERR: isValid is used before being assigned
+isValid = false;
+console.log(isValid); // Print: false
